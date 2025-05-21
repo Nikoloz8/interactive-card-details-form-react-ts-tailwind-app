a1c3eacef1d4fffcffc02be56d5ffc048e16fcc6
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-21 16:26:24 +0400

    Commit

--- src/components/CardBack.tsx
+++ src/components/CardBack.tsx
@@ -7,7 +7,7 @@ type CardBack = {
 
 export default function CardBack({textPreset1, form}:CardBack) {
     return (
-        <div className='max-lg:ml-[80px]! max-sm:ml-[50px]! max-md:px-[24px]! max-md:w-[305px]! max-md:h-[200px]! w-[447px] rounded-[10px] shadow-[0_39px_120px_0_#000000] h-[245px] bg-[url("./images/bg-card-back.png")] max-lg:m-0! ml-[70px]! flex items-center max-md:bg-cover max-md:bg-center justify-end p-[50px]! bg-no-repeat'>
+        <div className='max-lg:ml-[80px]! max-sm:ml-[50px]! max-md:px-[24px]! max-md:w-[305px]! max-md:h-[200px]! w-[447px] rounded-[10px] shadow-[0_39px_120px_0_#000000] h-[245px] bg-[url("/images/bg-card-back.png")] max-lg:m-0! ml-[70px]! flex items-center max-md:bg-cover max-md:bg-center justify-end p-[50px]! bg-no-repeat'>
             <h4 className={`${textPreset1} max-md:mb-[1px] max-md:ml-[0px] mb-[5px]`}>
                 {form.cvc ? `${form.cvc}${"0".repeat(3 - form.cvc.length)}` : "000"}
             </h4>
--- src/components/CardFront.tsx
+++ src/components/CardFront.tsx
@@ -11,7 +11,7 @@ type CardFront = {
 
 export default function CardFront({textPreset4, textPreset1, form}:CardFront) {
     return (
-        <div className='bg-no-repeat max-lg:mr-[20px]! max-md:p-[20px]! max-md:w-[305px]! max-md:h-[200px]! max-lg:mb-[60px]! max-lg:mt-[-136px]! max-md:mt-[-117px]! max-lg:order-[2] bg-[url("./images/bg-card-front.png")] max-md:bg-cover max-md:bg-center w-[447px] h-[245px] p-[35.5px]! flex flex-col justify-between items-start shadow-[0_5px_20 0px_0_#000000] rounded-[10px]'>
+        <div className='bg-no-repeat max-lg:mr-[20px]! max-md:p-[20px]! max-md:w-[305px]! max-md:h-[200px]! max-lg:mb-[60px]! max-lg:mt-[-136px]! max-md:mt-[-117px]! max-lg:order-[2] bg-[url("/images/bg-card-front.png")] max-md:bg-cover max-md:bg-center w-[447px] h-[245px] p-[35.5px]! flex flex-col justify-between items-start shadow-[0_5px_20 0px_0_#000000] rounded-[10px]'>
             <img src="/images/card-logo.svg" alt="" />
             <div className='flex flex-col gap-[12px] w-[100%]'>
                 <h3 className={`${textPreset4} max-md:text-[18px]`}>  {(
